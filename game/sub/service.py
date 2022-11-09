a2ec26f37becb0ac2d8df2d124785dd0e47a798f
SergiiPachkovskyi <SergiiPachkovskyi@gmail.com>
2022-11-09 09:48:40 +0200

    some edit

--- game/sub/service.py
+++ game/sub/service.py
@@ -1,12 +1,16 @@
+import environ
 import smtplib
 import ssl
 from email.message import EmailMessage
+
+env = environ.Env()
+environ.Env.read_env()
 
 
 def send(user_email, text, subject):
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.ukr.net", port=465, context=context) as server:
-        server.login("saintdaemon@ukr.net", '8s4XZdxQI7zhkz6H')
+        server.login("saintdaemon@ukr.net", env('EMAIL_PASSWORD'))
         msg = EmailMessage()
         msg['Subject'] = subject
         msg['From'] = 'saintdaemon@ukr.net'
