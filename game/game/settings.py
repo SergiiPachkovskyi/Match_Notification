2ec8a6a6641580760623491412c255e5f18358e4
SergiiPachkovskyi <SergiiPachkovskyi@gmail.com>
2022-11-16 08:52:14 +0200

    test

--- game/game/settings.py
+++ game/game/settings.py
@@ -91,6 +91,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': 'match_notification',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
